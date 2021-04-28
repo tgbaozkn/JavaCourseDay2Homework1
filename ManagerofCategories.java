653bbf99f7dc7344cc17faeab3089df88d2a364c
Tugba <39098806+tgbaozkn@users.noreply.github.com>
2021-04-28 17:59:42 +0300

    Create ManagerofCategories.java

---
+++ ManagerofCategories.java
@@ -0,0 +1,9 @@
+
+public class ManagerofCategories {
+	public void listofCategories(Categories[] categories) {
+		for(Categories category : categories) {
+			System.out.println(category.name);
+		}
+	}
+
+}
